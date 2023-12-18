1b236c7648e167d585e8b9671e141d0b731c1699
Ayush Sharma <ayushsharma830230@gmail.com>
2023-12-18 13:01:26 +0530

    env variables and appwrite authenctication service

--- src/appwrrite/auth.js
+++ src/appwrrite/auth.js
@@ -1,3 +1,5 @@
+//Making a Service for Authentication
+
 import conf from "../conf/conf";
 import { Client, Account, ID } from "appwrite";
 
@@ -5,12 +7,57 @@ export class AuthService {
     client = new Client();  //se docs->Authentication in appwrite for these 
     account;
 
-    constructor() {     //we w
+    constructor() {     //we want the client and account to be set up not directly in class but rather at time of object inititlaisation
         this.client
             .setEndpoint('conf.appwriteURL')                      // Your API Endpoint
             .setProject('conf.appwriteProductId');               // Your project ID
 
         account = new Account(client);
+    }
+
+    //Now making different methods for different purpose and underlying we will be using appwrite but later if we want to change appwrite then these methods remains same just their underlying defination changes
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, passwword, name)
+            if (userAccount) {
+                return this.login({ email, password });
+            }
+            else {
+                return userAccount;
+            }
+        }
+        catch (error) {
+            throw error;
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        }
+        catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        }
+        catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+        return null;    //in case of any error before try catch
+    }
+
+    async logout() {
+        try {
+            return await this.account.deleteSessions();
+        }
+        catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+        return null;    //in case of any error before try catch
     }
 }
 
